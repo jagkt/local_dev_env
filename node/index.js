b5b880edd01b95fe72933d0ab8b716d3502ac0f0
jagkt <insighttechnet@gmail.com>
2023-11-27 22:48:25 +0100

    file updated

--- node/index.js
+++ node/index.js
@@ -5,3 +5,7 @@ const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
     res.send("Hello from node_app");
 });
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
